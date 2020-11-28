b9527865b67b907c878195e9de44f06fcce1b8c4
HomeServing <xiangrui@aiursoft.com>
2020-11-28 20:26:03 +0800

    IHttpService

--- src/Abstracts/KCommon.Core.Abstract/Http/IHttpService.cs
+++ src/Abstracts/KCommon.Core.Abstract/Http/IHttpService.cs
@@ -7,30 +7,27 @@ namespace KCommon.Core.Abstract.Http
         // Get请求
         string Get<TRequestData>(string url, TRequestData data, bool forceHttp = false);
         Task<string> GetAsync<TRequestData>(string url, TRequestData data, bool forceHttp = false);
-        T GetToJson<T, TRequestData>(string url, TRequestData data, bool forceHttp = false);
-        Task<T> GetToJsonAsync<T, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        TResponseData GetToJson<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<TResponseData> GetToJsonAsync<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
         
         // Post请求
-        string PostForm(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        Task<string> PostFormAsync(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        T PostFormToJson<T>(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        Task<T> PostFormToJsonAsync<T>(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        
-        string PostJson(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        Task<string> PostJsonAsync(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        T PostJsonToJson<T>(HttpUrl url, HttpUrl postData, bool forceHttp = false);
-        Task<T> PostJsonToJsonAsync<T>(HttpUrl url, HttpUrl postData, bool forceHttp = false);
+        string Post<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<string> PostAsync<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        TResponseData PostFormToJson<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<TResponseData> PostFormToJsonAsync<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        TResponseData PostJsonToJson<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<TResponseData> PostJsonToJsonAsync<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
 
         // Put请求
-        string PutJson(HttpUrl url, HttpUrl putData, bool forceHttp = false);
-        Task<string> PutJsonAsync(HttpUrl url, HttpUrl putData, bool forceHttp = false);
-        T PutJsonToJson<T>(HttpUrl url, HttpUrl putData, bool forceHttp = false);
-        Task<T> PutJsonToJsonAsync<T>(HttpUrl url, HttpUrl putData, bool forceHttp = false);
+        string Put<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<string> PutAsync<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        TResponseData PutJsonToJson<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<TResponseData> PutJsonToJsonAsync<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
         
         // Delete请求
-        string Delete(HttpUrl url, HttpUrl deleteData, bool forceHttp = false);
-        Task<string> DeleteAsync(HttpUrl url, HttpUrl deleteData, bool forceHttp = false);
-        T DeleteToJson<T>(HttpUrl url, HttpUrl deleteData, bool forceHttp = false);
-        Task<T> DeleteToJsonAsync<T>(HttpUrl url, HttpUrl deleteData, bool forceHttp = false);
+        string Delete<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<string> DeleteAsync<TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        TResponseData DeleteJsonToJson<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
+        Task<TResponseData> DeleteJsonToJsonAsync<TResponseData, TRequestData>(string url, TRequestData data, bool forceHttp = false);
     }
 }
