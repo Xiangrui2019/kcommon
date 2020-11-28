09a77f31ba828ae6ecb67edb99219882d291258a
HomeServing <xiangrui@aiursoft.com>
2020-11-28 15:16:46 +0800

    t

---
+++ src/Impls/KCommon.Core/Utilities/ExceptionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KCommon.Core.Utilities
+{
+    public static class ExceptionHelper
+    {
+        public static void EatException(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        
+        public static T EatException<T>(Func<T> action, T defaultValue = default(T))
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
