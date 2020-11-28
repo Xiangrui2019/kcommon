6756f84ea35e2f0d55d617193b33da560c610904
HomeServing <xiangrui@aiursoft.com>
2020-11-29 07:28:15 +0800

    move equal from eurse to dataannon

---
+++ src/Impls/KCommon.Core/Extensions/ArrayExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KCommon.Core.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static byte[,] Copy(this byte[,] bytes)
+        {
+            int width = bytes.GetLength(0), height = bytes.GetLength(1);
+            byte[,] newBytes = new byte[width, height];
+            Array.Copy(bytes, newBytes, bytes.Length);
+            return newBytes;
+        }
+    }
+}
---
+++ src/Impls/KCommon.Core/Validations/Equal.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KCommon.Core.Validations
+{
+    public class Equal : ValidationAttribute
+    {
+        public object Excepted { get; set; }
+        
+        public override bool IsValid(object actual)
+        {
+            if (!actual.Equals(Excepted))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        protected override ValidationResult IsValid(object actual, ValidationContext validationContext)
+        {
+            if (IsValid(actual))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult($"The {validationContext.DisplayName} isn't same as {Excepted.GetType()}!");
+            }
+        }
+    }
+}
