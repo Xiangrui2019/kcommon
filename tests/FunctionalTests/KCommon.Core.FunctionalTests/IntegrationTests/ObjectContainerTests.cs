4ce31fa22073d3cbd7a7826ca9d576c12d68c259
HomeServing <xiangrui@aiursoft.com>
2020-11-29 15:25:50 +0800

    Add basic tests

---
+++ tests/FunctionalTests/KCommon.Core.FunctionalTests/IntegrationTests/ObjectContainerTests.cs
@@ -0,0 +1,34 @@
+﻿using KCommon.Core.Autofac;
+using KCommon.Core.Components;
+using KCommon.Core.Configurations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KCommon.Core.FunctionalTests.IntegrationTests
+{
+    [TestClass]
+    public class ObjectContainerTests
+    {
+        [TestMethod]
+        public void TestService()
+        {
+            Configuration
+               .Create()
+               .UseAutofac()
+               .AddCommonComponents()
+               .SetDefault<TestService, TestService>()
+               .BuildContainer();
+
+            var testService = ObjectContainer.Resolve<TestService>();
+
+            Assert.AreEqual(10, testService.Test());
+        }
+    }
+
+    public class TestService
+    {
+        public int Test()
+        {
+            return 10;
+        }
+    }
+}
