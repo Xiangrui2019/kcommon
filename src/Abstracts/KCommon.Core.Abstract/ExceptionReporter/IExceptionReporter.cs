f6dedaeff9513b53428e18386d3dd056df60e7f1
HomeServing <xiangrui@aiursoft.com>
2020-11-30 21:31:58 +0800

    Add Basic ExceptionReporter

---
+++ src/Abstracts/KCommon.Core.Abstract/ExceptionReporter/IExceptionReporter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KCommon.Core.Abstract.ExceptionReporter
+{
+    public interface IExceptionReporter
+    {
+        Task ReportAsync(Exception e, string path = "Inline");
+    }
+}
