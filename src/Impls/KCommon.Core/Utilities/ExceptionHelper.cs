ed5f7eac13a1f18cd11a86c24d418be27a1cdfc3
HomeServing <xiangrui@aiursoft.com>
2020-11-30 21:25:19 +0800

    Add CatchException Method

--- src/Impls/KCommon.Core/Utilities/ExceptionHelper.cs
+++ src/Impls/KCommon.Core/Utilities/ExceptionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace KCommon.Core.Utilities
 {
@@ -15,7 +16,19 @@ namespace KCommon.Core.Utilities
                 // ignored
             }
         }
-        
+
+        public static async Task EatExceptionAsync(Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
         public static T EatException<T>(Func<T> action, T defaultValue = default(T))
         {
             try
@@ -25,6 +38,42 @@ namespace KCommon.Core.Utilities
             catch (Exception)
             {
                 return defaultValue;
+            }
+        }
+
+        public static async Task<T> EatExceptionAsync<T>(Func<Task<T>> action, T defaultValue = default(T))
+        {
+            try
+            {
+                return await action();
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static Tuple<T, Exception> CatchException<T>(Func<T> action, T defaultValue = default(T))
+        {
+            try
+            {
+                return new Tuple<T, Exception>(action(), null);
+            }
+            catch (Exception e)
+            {
+                return new Tuple<T, Exception>(defaultValue, e);
+            }
+        }
+
+        public static async Task<Tuple<T, Exception>> CatchExceptionAsync<T>(Func<Task<T>> action, T defaultValue = default(T))
+        {
+            try
+            {
+                return new Tuple<T, Exception>(await action(), null);
+            }
+            catch (Exception e)
+            {
+                return new Tuple<T, Exception>(defaultValue, e);
             }
         }
     }
