4238fcd4a73aad08699361c91061c2ba588123cf
HomeServing <xiangrui@aiursoft.com>
2020-11-30 21:43:21 +0800

    Add Empty Exception Handler

---
+++ src/Impls/KCommon.Core/ExceptionReporter/EmptyExceptionReporter.cs
@@ -0,0 +1,14 @@
+﻿using KCommon.Core.Abstract.ExceptionReporter;
+using System;
+using System.Threading.Tasks;
+
+namespace KCommon.Core.ExceptionReporter
+{
+    public class EmptyExceptionReporter : IExceptionReporter
+    {
+        public Task ReportAsync(Exception e, string path = "Inline")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
