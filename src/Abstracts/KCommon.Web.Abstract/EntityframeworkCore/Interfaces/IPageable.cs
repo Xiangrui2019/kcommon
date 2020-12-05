d2f08f7cd97d55fbb0f155d5176ac7b25b0a4081
HomeServing <xiangrui@aiursoft.com>
2020-12-05 17:35:29 +0800

    EFTCCC

---
+++ src/Abstracts/KCommon.Web.Abstract/EntityframeworkCore/Interfaces/IPageable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCommon.Web.Abstract.EntityframeworkCore.Interfaces
+{
+    public interface IPageable
+    {
+        [Range(1, int.MaxValue)]
+        int PageNumber { get; set; }
+
+        [Range(1, int.MaxValue)]
+        int PageSize { get; set; }
+    }
+}
