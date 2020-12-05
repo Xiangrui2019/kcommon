53fd08b47fd1f0d36f77db7a61cc2a39e0e6200e
HomeServing <xiangrui@aiursoft.com>
2020-12-05 18:40:18 +0800

    Add Basic Test

--- src/Impls/KCommon.Web/Extensions/HostExtensions.cs
+++ src/Impls/KCommon.Web/Extensions/HostExtensions.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Polly;
-using System;
-using System.Data.SqlClient;
 
 namespace KCommon.Web.Extensions
 {
---
+++ tests/FunctionalTests/KCommon.Core.Webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KCommon.Core.Abstract.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KCommon.Core.Webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger _logger;
+
+        public WeatherForecastController(ILoggerFactory factory)
+        {
+            _logger = factory.Create(GetType().FullName);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.Error("Testing");
+
+
+            return Ok();
+        }
+    }
+}
