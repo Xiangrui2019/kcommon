f1ee049f5149c0ef1f9db932d50a714f6fc7e80e
HomeServing <xiangrui@aiursoft.com>
2020-12-05 21:18:16 +0800

    dayily stock

---
+++ src/Impls/KCommon.MicroServices.Consul/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Consul;
+using KCommon.Core.Abstract.Logging;
+using KCommon.Core.Configurations;
+using System;
+
+namespace KCommon.MicroServices.Consul
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>Use Consul as ServiceDiscovery
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseConsulServiceDiscovery(this Configuration configuration, Action<ConsulClientConfiguration> options)
+        {
+            var client = new ConsulClient(options);
+            configuration.SetDefault<ConsulClient, ConsulClient>(client);
+
+            return configuration;
+        }
+    }
+}
---
+++ src/Impls/KCommon.MicroServices.Consul/ConsulServiceResloverService.cs
@@ -0,0 +1,36 @@
+﻿using Consul;
+using KCommon.Core.Abstract.Cache;
+using KCommon.MicroServices.Abstract.ServiceDiscovery;
+using KCommon.MicroServices.Abstract.ServiceDiscovery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCommon.MicroServices.Consul
+{
+    public class ConsulServiceResloverService : IServiceResloverService
+    {
+        private readonly ConsulClient _client;
+        private readonly ICache _cache;
+
+        public ConsulServiceResloverService(
+            ConsulClient client,
+            ICache cache)
+        {
+            _client = client;
+            _cache = cache;
+        }
+
+        public async Task<Service> ResloveServiceAsync(string moduleName, string serviceName)
+        {
+            var service_entrys = await _cache.GetAndCacheAsync(
+                $"ServiceDiscovery-{moduleName}-{serviceName}",
+                async () => await GetServiceAsync(moduleName, serviceName), 5);
+        }
+
+        private async Task<QueryResult<ServiceEntry[]>> GetServiceAsync(string moduleName, string serviceName)
+            => await _client.Health.Service($"{moduleName}.{serviceName}", string.Empty, true);
+    }
+}
