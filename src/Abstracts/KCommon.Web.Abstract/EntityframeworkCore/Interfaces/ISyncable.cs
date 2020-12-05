322f81349edeb1116c1b74f6b23ea0a09b72688d
HomeServing <xiangrui@aiursoft.com>
2020-12-05 17:32:06 +0800

    RV

---
+++ src/Abstracts/KCommon.Web.Abstract/EntityframeworkCore/Interfaces/ISyncable.cs
@@ -0,0 +1,8 @@
+﻿namespace KCommon.Web.Abstract.EntityframeworkCore
+{
+    public interface ISyncable<T>
+    {
+        bool EqualsInDb(T obj);
+        T Map();
+    }
+}
---
+++ src/Impls/KCommon.Web/Extensions/HostExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace KCommon.Web.Extensions
+{
+    public static class HostExtensions
+    {
+        public static bool IsInKubernetes(this IHost webHost)
+        {
+            var cfg = (IConfiguration) webHost.Services.GetService(typeof(IConfiguration));
+
+            var orchestratorType = cfg.GetValue<string>("OrchestratorType");
+            return orchestratorType?.ToUpper() == "K8S";
+        }
+    }
+}
