9d0356b31af201d9474135711a51945ec130e2ee
HomeServing <xiangrui@aiursoft.com>
2020-12-05 19:27:36 +0800

    Redis Cache

---
+++ src/Impls/KCommon.Core.RedisCache/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using KCommon.Core.Abstract.Cache;
+using KCommon.Core.Configurations;
+using StackExchange.Redis;
+using System;
+
+namespace KCommon.Core.RedisCache
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>Use memeory as the cache.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseInRedisCache(this Configuration configuration, string connectionString)
+        {
+            configuration.SetDefault<ConnectionMultiplexer, ConnectionMultiplexer>(
+                ConnectionMultiplexer.Connect(connectionString));
+
+            configuration.SetDefault<ICache, InRedisCache>();
+
+            return configuration;
+        }
+    }
+}
---
+++ src/Impls/KCommon.Core.RedisCache/InRedisCache.cs
@@ -0,0 +1,90 @@
+﻿using KCommon.Core.Abstract.Cache;
+using KCommon.Core.Abstract.Serializing;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCommon.Core.RedisCache
+{
+    public class InRedisCache : ICache
+    {
+        private readonly IDatabase _cache;
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public InRedisCache(ConnectionMultiplexer factory)
+        {
+            _cache = factory.GetDatabase();
+        }
+
+        public void Clear(string cacheKey)
+        {
+            _cache.StringSet(cacheKey, "");
+        }
+
+        public T Get<T>(string cacheKey)
+        {
+            var result = _cache.StringGet(cacheKey);
+
+            if (result == "")
+            {
+                return default(T);
+            }
+
+            return JsonConvert.DeserializeObject<T>(result);
+        }
+
+        public async Task<T> GetAsync<T>(string cacheKey)
+        {
+            var result = await _cache.StringGetAsync(cacheKey);
+
+            if (result == "")
+            {
+                return default(T);
+            }
+
+            return JsonConvert.DeserializeObject<T>(result);
+        }
+
+        public T GetAndCache<T>(string cacheKey, Func<T> backup, int cachedMinutes = 20)
+        {
+            var result = Get<T>(cacheKey);
+
+            if (result == null)
+            {
+                result = backup();
+                Set<T>(cacheKey, result, cachedMinutes);
+            }
+
+            return result;
+        }
+
+        public async Task<T> GetAndCacheAsync<T>(string cacheKey, Func<Task<T>> backup, int cachedMinutes = 20)
+        {
+            var result = await GetAsync<T>(cacheKey);
+
+            if (result == null)
+            {
+                result = await backup();
+                await SetAsync<T>(cacheKey, result, cachedMinutes);
+            }
+
+            return result;
+        }
+
+        public void Set<T>(string cacheKey, T cacheValue, int cachedMinutes = 20)
+        {
+            var result = JsonConvert.SerializeObject(cacheValue);
+            _cache.StringSet(cacheKey, result, TimeSpan.FromMinutes(cachedMinutes));
+        }
+
+        public async Task SetAsync<T>(string cacheKey, T cacheValue, int cachedMinutes = 20)
+        {
+            var result = JsonConvert.SerializeObject(cacheValue);
+            await _cache.StringSetAsync(cacheKey, result, TimeSpan.FromMinutes(cachedMinutes));
+        }
+    }
+}
