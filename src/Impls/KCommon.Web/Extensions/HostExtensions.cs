08d29397b6cc13e352a96b7d0c31919e5c47f01f
HomeServing <xiangrui@aiursoft.com>
2020-12-05 17:38:15 +0800

    Add Auto migrate

--- src/Impls/KCommon.Web/Extensions/HostExtensions.cs
+++ src/Impls/KCommon.Web/Extensions/HostExtensions.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+using System;
+using System.Data.SqlClient;
 
 namespace KCommon.Web.Extensions
 {
@@ -11,6 +17,66 @@ namespace KCommon.Web.Extensions
 
             var orchestratorType = cfg.GetValue<string>("OrchestratorType");
             return orchestratorType?.ToUpper() == "K8S";
+        }
+
+        // 自动迁移数据库
+        public static IHost MigrateDbContext<TContext>(this IHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            var underK8s = webHost.IsInKubernetes();
+
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+                    if (underK8s)
+                    {
+                        InvokeSeeder(seeder, context, services);
+                    }
+                    else
+                    {
+                        var retries = 10;
+                        var retry = Policy.Handle<SqlException>()
+                            .WaitAndRetry(
+                                retryCount: retries,
+                                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                                onRetry: (exception, timeSpan, retry, ctx) =>
+                                {
+                                    logger.LogWarning(exception, "[{prefix}] Exception {ExceptionType} with message {Message} detected on attempt {retry} of {retries}", nameof(TContext), exception.GetType().Name, exception.Message, retry, retries);
+                                });
+
+                        //if the sql server container is not created on run docker compose this
+                        //migration can't fail for network related exception. The retry options for DbContext only 
+                        //apply to transient exceptions
+                        // Note that this is NOT applied when running some orchestrators (let the orchestrator to recreate the failing service)
+                        retry.Execute(() => InvokeSeeder(seeder, context, services));
+                    }
+
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                    if (underK8s)
+                    {
+                        throw;          // Rethrow under k8s because we rely on k8s to re-run the pod
+                    }
+                }
+            }
+
+            return webHost;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services)
+            where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
         }
     }
 }
