c144fcef5a830b5f9b642f03d4d24ab1ed65ed1b
HomeServing <xiangrui@aiursoft.com>
2020-12-06 14:08:41 +0800

    EDDD

---
+++ src/Impls/KCommon.EDDD/Configurations/EDDDConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using KCommon.Core.Components;
+using KCommon.Core.Configurations;
+using KCommon.Core.Validations;
+using System;
+
+namespace KCommon.EDDD.Configurations
+{
+    public class EDDDConfiguration
+    {
+        private readonly Configuration _configuration;
+
+        public static EDDDConfiguration Instance { get; private set; }
+
+        private EDDDConfiguration(Configuration configuration)
+        {
+            if (!new NotNull().IsValid(configuration)) throw new ArgumentNullException(nameof(configuration));
+
+            _configuration = configuration;
+        }
+
+        public static EDDDConfiguration CreateEDDD(Configuration configuration)
+        {
+            Instance = new EDDDConfiguration(configuration);
+            return Instance;
+        }
+
+        public Configuration GetCommonConfiguration()
+        {
+            return _configuration;
+        }
+
+        public EDDDConfiguration RegisterEDDDComponents()
+        {
+            return this;
+        }
+    }
+}
