3391440a3fd4cb8ceec225806e401e998f8fc3de
HomeServing <xiangrui@aiursoft.com>
2020-12-11 19:25:31 +0800

    object containter

---
+++ src/Abstracts/KCommon.Core.Abstract/Components/LifeStyle.cs
@@ -0,0 +1,16 @@
+﻿namespace KCommon.Core.Abstract.Components
+{
+    // 服务组件的生命周期
+    public enum LifeStyle
+    {
+        /// <summary>Represents a component is a transient component.
+        /// </summary>
+        Transient,
+        /// <summary>Represents a component is a scoped component.
+        /// </summary>
+        Scoped,
+        /// <summary>Represents a component is a singleton component.
+        /// </summary>
+        Singleton
+    }
+}
