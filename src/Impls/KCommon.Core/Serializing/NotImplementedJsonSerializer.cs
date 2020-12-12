f087cc656123cbe5be411fc597c19ce4d72af9c2
HomeServing <xiangrui@aiursoft.com>
2020-12-12 15:04:01 +0800

    T

---
+++ src/Impls/KCommon.Core/Serializing/NotImplementedJsonSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using KCommon.Core.Abstract.Serializing;
+
+namespace KCommon.Core.Serializing
+{
+    public class NotImplementedJsonSerializer : IJsonSerializer
+    {
+        public string Serialize(object obj)
+        {
+            throw new NotSupportedException(string.Format("{0} does not support serializing object.", GetType().FullName));
+        }
+        
+        public object Deserialize(string value, Type type)
+        {
+            throw new NotSupportedException(string.Format("{0} does not support deserializing object.", GetType().FullName));
+        }
+        
+        public T Deserialize<T>(string value) where T : class
+        {
+            throw new NotSupportedException(string.Format("{0} does not support deserializing object.", GetType().FullName));
+        }
+    }
+}
