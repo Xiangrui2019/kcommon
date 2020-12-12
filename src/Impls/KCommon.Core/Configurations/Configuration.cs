9e4abdafa8a0a2d3aa4782e067a62e85e2ccc690
HomeServing <xiangrui@aiursoft.com>
2020-12-12 09:30:27 +0800

    add configuration

---
+++ src/Impls/KCommon.Core/Configurations/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using KCommon.Core.Abstract.Components;
+using KCommon.Core.Components;
+
+namespace KCommon.Core.Configurations
+{
+    public class Configuration
+    {
+        /// <summary>Provides the singleton access instance.
+        /// </summary>
+        public static Configuration Instance { get; private set; }
+
+        private Configuration() { }
+
+        public static Configuration Create()
+        {
+            Instance = new Configuration();
+            return Instance;
+        }
+
+        public Configuration SetDefault<TService, TImplementer>(string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            ObjectContainer.Register<TService, TImplementer>(serviceName, life);
+            return this;
+        }
+        public Configuration SetDefault<TService, TImplementer>(TImplementer instance, string serviceName = null)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            ObjectContainer.RegisterInstance<TService, TImplementer>(instance, serviceName);
+            return this;
+        }
+
+        public Configuration RegisterCommonComponents()
+        {
+            return this;
+        }
+        
+        public Configuration BuildContainer()
+        {
+            ObjectContainer.Build();
+            return this;
+        }
+    }
+}
