41fd12c12fcb8898635423225ec2a7de82b035a1
HomeServing <xiangrui@aiursoft.com>
2020-12-12 08:26:22 +0800

    add objectcontainer test

---
+++ tests/UnitTests/KCommon.Core.UnitTests/Components/ObjectContainerTest.cs
@@ -0,0 +1,17 @@
+﻿using KCommon.Core.Components;
+using KCommon.Core.UnitTests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KCommon.Core.UnitTests.Components
+{
+    [TestClass]
+    public class ObjectContainerTest
+    {
+        [TestMethod]
+        public void TestBasic()
+        {
+            ObjectContainer.SetContainer(new MockObjectContainer());
+            ObjectContainer.Build();
+        }
+    }
+}
---
+++ tests/UnitTests/KCommon.Core.UnitTests/Mocks/MockObjectContainer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using KCommon.Core.Abstract.Components;
+
+namespace KCommon.Core.UnitTests.Mocks
+{
+    public class MockObjectContainer : IObjectContainer
+    {
+        public bool isBuilded { get; set; } = false;
+        
+        public void Build()
+        {
+            isBuilded = true;
+        }
+
+        public void RegisterType(Type implementationType, string serviceName = null, LifeStyle life = LifeStyle.Singleton)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterType(Type serviceType, Type implementationType, string serviceName = null,
+            LifeStyle life = LifeStyle.Singleton)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TService, TImplementer>(string serviceName = null, LifeStyle life = LifeStyle.Singleton) where TService : class where TImplementer : class, TService
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterInstance<TService, TImplementer>(TImplementer instance, string serviceName = null) where TService : class where TImplementer : class, TService
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryResolve<TService>(out TService instance) where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryResolve(Type serviceType, out object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService ResolveNamed<TService>(string serviceName) where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ResolveNamed(string serviceName, Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryResolveNamed(string serviceName, Type serviceType, out object instance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
