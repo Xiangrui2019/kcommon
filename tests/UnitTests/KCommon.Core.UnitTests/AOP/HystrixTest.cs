b95b8e3afa94bb77ca42ffd45937ce54f1346b67
HomeServing <xiangrui@aiursoft.com>
2020-12-12 21:16:33 +0800

    test

---
+++ tests/UnitTests/KCommon.Core.UnitTests/AOP/HystrixTest.cs
@@ -0,0 +1,45 @@
+﻿using KCommon.Core.Autofac;
+using KCommon.Core.Components;
+using KCommon.Core.Configurations;
+using KCommon.Core.UnitTests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KCommon.Core.UnitTests.AOP
+{
+    [TestClass]
+    public class HystrixTest
+    {
+        private AopMockService _service;
+        
+        [TestInitialize]
+        public void InitFramework()
+        {
+            Configuration
+                .Create()
+                .UseAutofac()
+                .RegisterCommonComponents()
+                .SetDefault<AopMockService, AopMockService>()
+                .BuildContainer();
+
+            _service = ObjectContainer.Resolve<AopMockService>();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _service.HelloAsync("Test")
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var s = _service.Add(1, 2);
+            Assert.AreEqual(s, 3);
+
+            s = _service.AddTest(1, 2);
+            Assert.AreEqual(s, 0);
+        }
+    }
+}
---
+++ tests/UnitTests/KCommon.Core.UnitTests/Mocks/AopMockService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using KCommon.Core.AOP;
+
+namespace KCommon.Core.UnitTests.Mocks
+{
+    public class AopMockService
+    {
+        [HystrixCommand(nameof(Hello1FallBackAsync), MaxRetryTimes = 3, EnableCircuitBreaker = true)]
+        public virtual async Task<string> HelloAsync(string name)//需要是虚方法
+        {
+            Console.WriteLine("尝试执行HelloAsync" + name);
+            String s = null;
+            s.ToString();
+            return "ok" + name;
+        }
+
+        [HystrixCommand(nameof(Hello2FallBackAsync))]
+        public virtual async Task<string> Hello1FallBackAsync(string name)
+        {
+            Console.WriteLine("Hello降级1" + name);
+            String s = null;
+            s.ToString();
+            return "fail_1";
+        }
+
+        public virtual async Task<string> Hello2FallBackAsync(string name)
+        {
+            Console.WriteLine("Hello降级2" + name);
+
+            return "fail_2";
+        }
+
+        [HystrixCommand(nameof(AddFall))]
+        public virtual int Add(int i, int j)
+        {
+            String s = null;
+            //s.ToString();
+            return i + j;
+        }
+        public int AddFall(int i, int j)
+        {
+            return 0;
+        }
+        
+        [HystrixCommand(nameof(AddTestFall))]
+        public virtual int AddTest(int i, int j)
+        {
+            String s = null;
+            s.ToString();
+            return i + j;
+        }
+        public int AddTestFall(int i, int j)
+        {
+            return 0;
+        }
+
+        [HystrixCommand(nameof(TestFallBack))]
+        public virtual void Test(int i)
+        {
+            Console.WriteLine("Test" + i);
+        }
+
+        public virtual void TestFallBack(int i)
+        {
+            Console.WriteLine("Test" + i);
+        }
+    }
+}
