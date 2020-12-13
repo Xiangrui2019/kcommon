1b3dd74450cc6bef90d17330de1eb9a6b2df2767
HomeServing <xiangrui@aiursoft.com>
2020-12-13 15:13:58 +0800

    add autocache strict

--- src/Impls/KCommon.Core/AOP/AutoCacheAttribute.cs
+++ src/Impls/KCommon.Core/AOP/AutoCacheAttribute.cs
@@ -1,13 +1,36 @@
-﻿using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading.Tasks;
 using AspectCore.DynamicProxy;
+using KCommon.Core.Abstract.Caching;
+using KCommon.Core.Components;
 
 namespace KCommon.Core.AOP
 {
     public class AutoCacheAttribute : AbstractInterceptorAttribute
     {
-        public override Task Invoke(AspectContext context, AspectDelegate next)
+        /// <summary>
+        /// 缓存时间, 0为不缓存
+        /// </summary>
+        public int CachedMinutes { get; set; } = 0;
+        
+        private static ICache _cache = ObjectContainer.Resolve<ICache>();
+        
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
         {
-            throw new System.NotImplementedException();
+            var cacheKey = GetCacheKey(context.ServiceMethod, context.Parameters);
+            var (result, status) = await _cache.TryGetAsync<object>(cacheKey);
+
+            if (status == true)
+            {
+                context.ReturnValue = result;
+                return;
+            }
+            
+            await next(context);
+            await _cache.SetAsync(cacheKey, context.ReturnValue, CachedMinutes);
         }
+
+        private string GetCacheKey(MethodInfo methodInfo, object[] param)
+            => $"AOP_AutoCache_{methodInfo.Name}_{methodInfo.DeclaringType}_{methodInfo}_{string.Join("_", param)}";
     }
 }
---
+++ tests/UnitTests/KCommon.Core.UnitTests/AOP/CacheTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using KCommon.Core.AOP;
+using KCommon.Core.Autofac;
+using KCommon.Core.Components;
+using KCommon.Core.Configurations;
+using KCommon.Core.MemoryCache;
+using KCommon.Core.UnitTests.Mocks;
+using KCommon.Core.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KCommon.Core.UnitTests.AOP
+{
+    [TestClass]
+    public class CacheTest
+    {
+        private TestService _service;
+
+        [TestInitialize]
+        public void InitFramework()
+        {
+            Configuration
+                .Create()
+                .UseAutofac()
+                .RegisterCommonComponents()
+                .UseInMemoryCache()
+                .SetDefault<TestService, TestService>()
+                .BuildContainer();
+
+            _service = ObjectContainer.Resolve<TestService>();
+        }
+        
+        [TestMethod]
+        public void TestBasic()
+        {
+            var t1 = _service.Rand();
+            var t2 = _service.Rand();
+            
+            Assert.AreEqual(t1, t2);
+        }
+
+        [TestMethod]
+        public void TestTimeout()
+        {
+            var t1 = _service.Rand();
+            var t1_t = _service.Rand();
+            Thread.Sleep(TimeSpan.FromMinutes(1));
+            var t2 = _service.Rand();
+            
+            Assert.AreNotEqual(t1, t2);
+            Assert.AreEqual(t1, t1_t);
+        }
+    }
+
+    public class TestService
+    {
+        [AutoCache(CachedMinutes = 1)]
+        public virtual ObjectId Rand()
+        {
+            return ObjectId.GenerateNewId();
+        }
+    }
+}
