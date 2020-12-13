0b072973b0dee0057a91beab86249088814bd7ec
HomeServing <xiangrui@aiursoft.com>
2020-12-13 11:21:07 +0800

    T

---
+++ tests/UnitTests/KCommon.Core.UnitTests/Serializing/MessagePackSerializerTest.cs
@@ -0,0 +1,48 @@
+﻿using MessagePack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessagePackSerializer = KCommon.Core.MessagePack.MessagePackSerializer;
+
+namespace KCommon.Core.UnitTests.Serializing
+{
+    [TestClass]
+    public class MessagePackSerializerTest
+    {
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var serializer = new MessagePackSerializer();
+
+            var r = serializer.Serialize(new V
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(r, null);
+        }
+        
+        [TestMethod]
+        public void TestDeSerialize()
+        {
+            var serializer = new MessagePackSerializer();
+
+            var t = serializer.Serialize(new V
+            {
+                A = 10,
+                B = 20,
+            });
+            var r = serializer.Deserialize<V>(t);
+            
+            Assert.AreEqual(r.A, 10);
+        }
+    }
+    
+    [MessagePackObject]
+    public class V
+    {
+        [Key(0)]
+        public int A { get; set; }
+        [Key(1)]
+        public int B { get; set; }
+    }
+}
---
+++ tests/UnitTests/KCommon.Core.UnitTests/Serializing/ProtoBufSerializerTest.cs
@@ -0,0 +1,49 @@
+﻿using KCommon.Core.ProtoBuf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+
+namespace KCommon.Core.UnitTests.Serializing
+{
+    [TestClass]
+    public class ProtoBufSerializerTest
+    {
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var serializer = new ProtoBufSerializer();
+
+            var t = serializer.Serialize(new VPb
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(t, null);
+        }
+        
+        [TestMethod]
+        public void TestDeSerialize()
+        {
+            var serializer = new ProtoBufSerializer();
+
+            var t = serializer.Serialize(new VPb
+            {
+                A = 10,
+                B = 20,
+            });
+
+            var r = serializer.Deserialize<VPb>(t);
+            
+            Assert.AreEqual(r.A, 10);
+        }
+    }
+    
+    [ProtoContract]
+    public class VPb
+    {
+        [ProtoMember(1)]
+        public int A { get; set; }
+        [ProtoMember(2)]
+        public int B { get; set; }
+    }
+}
