41730d62ba857d60886c2152fec8d3ff2b102b1d
HomeServing <xiangrui@aiursoft.com>
2020-12-13 11:02:51 +0800

    text.json

---
+++ src/Impls/KCommon.Core.ProtoBuf/ConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using KCommon.Core.Abstract.Serializing;
+using KCommon.Core.Configurations;
+
+namespace KCommon.Core.ProtoBuf
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>Use ProtobufNET as the protobuf serializer.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseTextJson(this Configuration configuration)
+        {
+            configuration.SetDefault<IProtobufSerializer, ProtoBufSerializer>(new ProtoBufSerializer());
+            return configuration;
+        }
+    }
+}
---
+++ src/Impls/KCommon.Core.ProtoBuf/ProtoBufSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using KCommon.Core.Abstract.Serializing;
+using ProtoBuf;
+
+namespace KCommon.Core.ProtoBuf
+{
+    /// <summary>ProtoBuf-NET implementationof IProtobufSerializer.
+    /// </summary>
+    public class ProtoBufSerializer : IProtobufSerializer
+    {
+        public byte[] Serialize(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            
+            using var memoryStream = new MemoryStream();
+            Serializer.Serialize(memoryStream, obj);
+
+            return memoryStream.ToArray();
+        }
+
+        public T Deserialize<T>(byte[] data) where T : class
+        {
+            using var memoryStream = new MemoryStream(data);
+            
+            return Serializer.Deserialize<T>(memoryStream);
+        }
+    }
+}
