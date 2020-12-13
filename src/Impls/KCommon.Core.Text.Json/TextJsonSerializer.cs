61a5b30f68938f2e484814a5c0af36d1722938de
HomeServing <xiangrui@aiursoft.com>
2020-12-13 08:44:21 +0800

    TS

--- src/Impls/KCommon.Core.Text.Json/TextJsonSerializer.cs
+++ src/Impls/KCommon.Core.Text.Json/TextJsonSerializer.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
 using KCommon.Core.Abstract.Serializing;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace KCommon.Core.Text.Json
 {
@@ -13,25 +8,13 @@ namespace KCommon.Core.Text.Json
     /// </summary>
     public class TextJsonSerializer : IJsonSerializer
     {
-        public JsonSerializerSettings Settings { get; private set; }
-
-        public TextJsonSerializer()
-        {
-            Settings = new JsonSerializerSettings
-            {
-                Converters = new List<JsonConverter> {new IsoDateTimeConverter()},
-                ContractResolver = new CustomContractResolver(),
-                ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
-            };
-        }
-
         /// <summary>Serialize an object to json string.
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
         public string Serialize(object obj)
         {
-            return obj == null ? null : JsonConvert.SerializeObject(obj, Settings);
+            return obj == null ? null : JsonSerializer.Serialize(obj);
         }
 
         /// <summary>Deserialize a json string to an object.
@@ -41,7 +24,7 @@ namespace KCommon.Core.Text.Json
         /// <returns></returns>
         public object Deserialize(string value, Type type)
         {
-            return JsonConvert.DeserializeObject(value, type, Settings);
+            return JsonSerializer.Deserialize(value, type);
         }
 
         /// <summary>Deserialize a json string to a strong type object.
@@ -51,22 +34,7 @@ namespace KCommon.Core.Text.Json
         /// <returns></returns>
         public T Deserialize<T>(string value) where T : class
         {
-            return JsonConvert.DeserializeObject<T>(JObject.Parse(value).ToString(), Settings);
-        }
-
-        private class CustomContractResolver : DefaultContractResolver
-        {
-            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
-            {
-                var jsonProperty = base.CreateProperty(member, memberSerialization);
-                if (jsonProperty.Writable) return jsonProperty;
-                var property = member as PropertyInfo;
-                if (property == null) return jsonProperty;
-                var hasPrivateSetter = property.GetSetMethod(true) != null;
-                jsonProperty.Writable = hasPrivateSetter;
-
-                return jsonProperty;
-            }
+            return JsonSerializer.Deserialize<T>(value);
         }
     }
 }
---
+++ tests/UnitTests/KCommon.Core.UnitTests/Serializing/JsonSerializerTest.cs
@@ -0,0 +1,79 @@
+﻿using KCommon.Core.JsonNet;
+using KCommon.Core.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KCommon.Core.UnitTests.Serializing
+{
+    [TestClass]
+    public class JsonSerializerTest
+    {
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var jsonSerializer = new TextJsonSerializer();
+
+            var r = jsonSerializer.Serialize(new ValueObject
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(r, null);
+        }
+        
+        [TestMethod]
+        public void TestSerializeJsonNet()
+        {
+            var jsonSerializer = new NewtonsoftJsonSerializer();
+
+            var r = jsonSerializer.Serialize(new ValueObject
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(r, null);
+        }
+        
+        [TestMethod]
+        public void TestDeSerialize()
+        {
+            var jsonSerializer = new TextJsonSerializer();
+
+            var r = jsonSerializer.Serialize(new ValueObject
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(r, null);
+
+            var s = jsonSerializer.Deserialize<ValueObject>(r);
+            
+            Assert.AreEqual(s.A, 10);
+        }
+        
+        [TestMethod]
+        public void TestDeSerializeJsonNet()
+        {
+            var jsonSerializer = new NewtonsoftJsonSerializer();
+
+            var r = jsonSerializer.Serialize(new ValueObject
+            {
+                A = 10,
+                B = 20,
+            });
+            
+            Assert.AreNotEqual(r, null);
+            
+            var s = jsonSerializer.Deserialize<ValueObject>(r);
+            Assert.AreEqual(s.A, 10);
+        }
+    }
+
+    public class ValueObject
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+}
